580647e080d7260990c75ff7e055e331b4363405
Lingzi <susanzhang0521@gmail.com>
2022-09-03 16:10:38 -0400

    comment

--- server/router/upload.js
+++ server/router/upload.js
@@ -3,6 +3,13 @@ const router = express.Router()
 
 const api_key = '2b108jwkqQofJmQ1nbXABTe'
 
+// service: https://my-api.plantnet.org/v2/identify/all
+// api-key: api-key=2b108jwkqQofJmQ1nbXABTe
+// image_1: images=https%3A%2F%2Fmy.plantnet.org%2Fimages%2Fimage_1.jpeg
+// image_2: images=https%3A%2F%2Fmy.plantnet.org%2Fimages%2Fimage_2.jpeg
+// organ_1: organs=flower
+// organ_2: organs=leaf
+
 router.get('/remote', async (req, res, next) => {
     const { image } = req.body
     try {
